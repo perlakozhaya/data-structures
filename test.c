4c44e8f71d7f06cbed47e063c494d43b09c5e593
Perla Kozhaya <kozhaya.perla@gmail.com>
2024-05-03 18:45:36 +0300

    Testing arrays as parameters

---
+++ test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define rows 3
+#define cols 2
+
+void transposeMatrix(int row, int col, int matrix[rows][cols]) {
+    int transposedMatrix[col][row];
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            transposedMatrix[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("Transposed Matrix:\n");
+    for (int i = 0; i < col; i++) {
+        for (int j = 0; j < row; j++) {
+            printf("%d ", transposedMatrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[rows][cols] = {
+        {1, 2},
+        {3, 4},
+        {5, 6}
+    };
+
+    printf("Original Matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    transposeMatrix(rows, cols, matrix);
+}
