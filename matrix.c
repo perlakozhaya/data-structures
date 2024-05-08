caaa2c1d69591d99d5e7603dc1e0119d4d7bbfe7
Perla Kozhaya <kozhaya.perla@gmail.com>
2024-05-08 14:16:53 +0300

    Moved upperTriangular() from test.c to matrix.c

--- matrix.c
+++ matrix.c
@@ -108,23 +108,35 @@ void selfProduct(int size, int matrice[][size], int degre) {
     displayMatrix(size, size, p);
 }
 
-// 6. Simplification d'une matrice
-void simplify(int row, int col, int m[row][col]) {
-    int tmp1, tmp2;
-    for(int i = 0; i < row; i++) {
-        for(int j = i+1; j < col; j++) {
-            for(int k = 0; k < row; k++) {
-                tmp2 = m[j][k] * m[0][i];
-                tmp1 = m[0][k] * m[j][i];
-
-                m[j][i] = tmp2 - tmp1;
-
-                printf("%d ", m[j][i]);
+// 6. Simplification d'une matrice en une forme triangulaire superieure
+void upperTriangular(int size, double m[size][size]) {
+    int tmp;
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            // ta ma tse2eb division par 0 baddalet bel awwal el ligne matrah ma m[i][i] == 0 ma3 el ligne li tahta ka2ano L1 <-> L2
+            if(m[i][i] == 0) {
+                for(int l = 0; l < size; l++) {
+                    tmp = m[i][l];
+                    m[i][l] = m[i+1][l];
+                    m[i+1][l] = tmp;
+                }
             }
-            printf("\n");
+            // l facteur double ta ma ta3mel arrondissement lal jaweb 
+            double facteur = m[j][i] / m[i][i]; // m[j][i] -> element yali bado ysir 0; m[i][i] element yali faw2o bi sater
+
+            for (int k = i; k < size; k++) {
+                m[j][k] -= facteur * m[i][k]; // ka2an L2 <- L2 - (facteur x L1)
+            }
         }
     }
-    // displayMatrix(row, col, m);
+
+    // Print the result
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%.2f\t", m[i][j]); // %.2f kermel to3rodle bas 2 chiffres apres la virgule
+        }
+        printf("\n");
+    }
 }
 
 int main() {
@@ -145,7 +157,15 @@ int main() {
         {1, 3}
     };
 
+    double m4[3][3] = {
+        {1, 0, 0},
+        {0, 0, 2},
+        {0, 3, 4}
+    };
+
     // transpose(3, 2, m2);
     // product(3, 2, 2, 2, m2, m3);
-    simplify(3, 3, m1);
+    // upperTriangular(3, m4);
+
+    return 0;
 }
