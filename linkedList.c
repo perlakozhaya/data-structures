6d9fad76055f18aa1ab80ccae6657c2b17bba2cd
RichardAkiki <richardakiki001@gmail.com>
2024-05-29 22:38:46 +0300

    create linkedList

--- linkedList.c
+++ linkedList.c
@@ -1,36 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the cellule struct
-typedef struct cellule {
+// Define the node struct
+typedef struct node {
     int info;
-    struct cellule *suivant;
-} cellule;
+    struct node *suivant;
+} node;
 
 // Function to create a linked list from an array
+node* createList(int t[], int size){
+    node *list = NULL;
+    node *newNode, *last = NULL;
 
+    for(int i = 0; i < size; i++){
+        newNode = (node*)malloc(sizeof(node));
+        newNode -> info = t[i];
+        newNode -> suivant = NULL;
 
+        if(list == NULL){
+            list = newNode;
+        }else{
+            last -> suivant = newNode;
+        }
+        last = newNode;
+    }
+    return list;
+}
 // Function to print the linked list
+void printList(node *list){
+    node *current = list;
 
+    while(current != NULL){
+        printf("%d ", current -> info);
+        current = current -> suivant;
+    }
+    printf("\n");
+}
+
+void freeList(node *list){
+    node *temp;
+    while (list != NULL) {
+        temp = list;
+        list = list->suivant;
+        free(temp);
+    }
+}
 
 // Main function to test the creer function
 int main() {
     int V[] = {1, 2, 3, 4, 5};
     int Nb = sizeof(V) / sizeof(V[0]);
 
-    cellule *liste = creer(V, Nb);
+    node *list = createList(V, Nb);
 
-    if (liste != NULL) {
-        printList(liste);
+    if (list != NULL) {
+        printList(list);
     }
 
     // Free the allocated memory
-    cellule *temp;
-    while (liste != NULL) {
-        temp = liste;
-        liste = liste->suivant;
-        free(temp);
-    }
+    freeList(list);
 
     return 0;
 }
